58653da97edfb0cc1bb2b51db207d36851131f73
Евгений <jenya.kuzya@mail.ru>
2020-10-24 16:55:52 +0300

    Beginning

---
+++ NetCore_Mentoring.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NetCore_Mentoring.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ NetCore_Mentoring.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NetCore_Mentoring.DAL
+{
+    public class Class1
+    {
+    }
+}
